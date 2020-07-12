56bd0a2eb1d5b60f51fdcde94e9f79f1affdb337
isabel985 <lee.isabel59@gmail.com>
2020-07-12 12:20:05 -0500

    refactored code to use map function

--- static/js/flickr.js
+++ static/js/flickr.js
@@ -1,13 +1,15 @@
 const loadData = (url) => {
     $.get(url, data => {
         let photosRow = $("#photos-row");
-        for (let i = 0; i < 101; i++) {
-            photosRow.append(`<div class="photo"><img src="https://farm${data.photos.photo[i].farm}.staticflickr.com/${data.photos.photo[i].server}/${data.photos.photo[i].id}_${data.photos.photo[i].secret}.jpg"/></div>`);
-        };
+        let pictures = data.photos.photo
+
+        pictures.map((item) => {
+            photosRow.append(`<div class="photo"><img src="https://farm${item.farm}.staticflickr.com/${item.server}/${item.id}_${item.secret}.jpg"/></div>`);
+        })
     });
 };
 
-$("#tag-form").on("submit", event => {
+$("#tag-form").submit(event => {
     event.preventDefault();
     $("#photos-row").empty();
 
@@ -15,6 +17,5 @@ $("#tag-form").on("submit", event => {
     var url = `https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=0f93d0daa4c0af5da43c95e700797464&tags=${tag}&format=json&nojsoncallback=1`;
 
     loadData(url);
-
     $("#tag-name").val("")
 });
